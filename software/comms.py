cf9b3c7355674779e1ebc04172d4ddb29576c084
Lucas Sta Maria <lucas.stamaria@gmail.com>
2021-02-13 23:22:22 +0800

    fix: organize comms.py to make OOP
    Move the global variable to be instance variables, mainly. `NUM_BYTES`
    is a global constant, so it does not need to be moved. Some methods
    (including __init__) were also missing `self`.
    
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- software/comms.py
+++ software/comms.py
@@ -14,42 +14,37 @@ import camera
 
 from queue import Queue
 
-USB = ""
-PACKET = ""
 NUM_BYTES = 3
-ser = None
-cam = None
-ctp = None
-
-q = Queue()
 
 class Communications:
-    def __init__(usb, camera_id1, camera_id2, SCREEN_DIMENSIONS, FPS):
-        USB = usb
-        cam = (camera.Camera(camera_id1), camera.Camera(camera_id2))
-        ser = serial.Serial(str(USB), 230400)
-        ctp = convert_to_packet.Convert_To_Packet()
-        #gui.GUI(cam, SCREEN_DIMENSIONS, FPS, CAM_ID) ###
+    def __init__(self, usb, camera_id1, camera_id2, SCREEN_DIMENSIONS, FPS):
+        self.USB = usb
+        self.cam = (camera.Camera(camera_id1), camera.Camera(camera_id2))
+        self.ser = serial.Serial(str(self.USB), 230400)
+        self.ctp = convert_to_packet.Convert_To_Packet()
+        #gui.GUI(self.cam, SCREEN_DIMENSIONS, FPS, CAM_ID) ###
+        self.q = Queue()
+        self.PACKET = ""
     
     def kill_op(self):
         self.encode_and_send('9' + chr(254) + chr(255))
     
     def receive(self, keys):
-        return self.receive_packets(ctp.pack(keys))
+        return self.receive_packets(self.ctp.pack(keys))
         
-    def encode_and_send(packets):
+    def encode_and_send(self, packets):
         for c in packets:
-            ser.write(c.encode("latin"))
+            self.ser.write(c.encode("latin"))
     
     def receive_packets(self, packets):
         line = ""
         if(packets[0] == '8'):
             if(packets[1] == chr(12)):
-                cam[0].collect_screenshot() ###
-                cam[1].collect_screenshot()
+                self.cam[0].collect_screenshot() ###
+                self.cam[1].collect_screenshot()
             else:
                 self.encode_and_send(packets) ###
-                line = ser.read(NUM_BYTES)
+                line = self.ser.read(NUM_BYTES)
                 
         elif(packets[0] == '9'):
             self.kill_op()
@@ -66,7 +61,7 @@ class Communications:
             self.receive_packets('8' + chr(10) + chr(255))
             self.receive_packets('8' + chr(11) + chr(255))
             
-            while not q.empty():
-                self.receive(q.get())
+            while not self.q.empty():
+                self.receive(self.q.get())
             
             # Take camera pic for GUI
