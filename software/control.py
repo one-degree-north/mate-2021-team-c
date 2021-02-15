ad79a0c69d6df9b562f2ac7d949e7d3ad8bfd16e
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-02-15 14:58:12 +0800

    Update control.py
    Added exit_program instance to controls

--- software/control.py
+++ software/control.py
@@ -8,15 +8,14 @@ Created on Tue Jan 26 16:08:11 2021
 
 import pygame
 import keyboard
-import exit_program
-
 
 KEYS = []
 
 class Control:
-    def __init__(self, comm):
+    def __init__(self, comm, exit):
         #key_input = pygame.key.get_pressed()
         self.comms = comm
+        self.exit_program = exit
         self.events = None
         self.key_input = None
         pygame.init()
@@ -33,4 +32,4 @@ class Control:
             
             for event in self.events:
                 if event.type == pygame.QUIT:
-                    exit_program.Exit()
+                    self.exit_program.Exit()
