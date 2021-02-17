f32d1c90b34b1f96fcebfff080cc18ff4030c973
Lucas Sta Maria <lucas.stamaria@gmail.com>
2021-02-17 16:19:00 +0800

    fix: instantiate and attach motors during setup
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -2,10 +2,27 @@
 #include <Servo.h>
 #include <Arduino.h>
 
+#define SPEED_MIN 1000
+#define SPEED_MID 1500
 #define SPEED_MAX 2000
 
+#define FRONT_LEFT_PIN 9
+#define FRONT_RIGHT_PIN 10
+#define REAR_LEFT_PIN 11
+#define REAR_RIGHT_PIN 12
+#define FORWARD_LEFT_PIN 13
+#define FORWARD_RIGHT_PIN 14
+
+Servo front_left;
+Servo front_right;
+Servo rear_left;
+Servo rear_right;
+Servo forward_left;
+Servo forward_right;
 
 void setup() {
+  setup_motors();
+  delay(2000);
   Serial.begin(115200);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
@@ -27,6 +44,15 @@ void loop() {
       flush_buffer(buffer, &buf_ind);
     }
   }
+}
+
+void setup_motors() {
+  front_left.attach(FRONT_LEFT_PIN);
+  front_right.attach(FRONT_RIGHT_PIN);
+  rear_left.attach(REAR_LEFT_PIN);
+  rear_right.attach(REAR_RIGHT_PIN);
+  forward_left.attach(FORWARD_LEFT_PIN);
+  forward_right.attach(FORWARD_RIGHT_PIN);
 }
 
 void write_to_buffer(int* buffer, int* buf_ind) {
