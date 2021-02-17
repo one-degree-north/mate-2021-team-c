f80b98716b410504987bab922fde48211c1a9557
Lucas Sta Maria <lucas.stamaria@gmail.com>
2021-02-17 16:01:23 +0800

    fix: remove Adafruit-specific Arduino code
    Signed-off-by: Lucas Sta Maria <lucas.stamaria@gmail.com>

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,40 +1,14 @@
-#include <Adafruit_ICM20649.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <Servo.h>
 #include <Arduino.h>
-#include "Adafruit_ZeroTimer.h"
-
-#if defined(LED_BUILTIN)
-#define pin  LED_BUILTIN
-#else
-#define pin  D2
-#endif
 
 #define SPEED_MAX 2000
 
-#define motorPin 10
-
-Adafruit_ICM20649 icm;
-uint16_t measurement_delay_us = 65535; // Delay between measurements for testing
-// For SPI mode, we need a CS pin
-#define ICM_CS 10
-// For software-SPI mode we need SCK/MOSI/MISO pins
-#define ICM_SCK 13
-#define ICM_MISO 12
-#define ICM_MOSI 11
-
-// timer tester
-Adafruit_ZeroTimer zt3 = Adafruit_ZeroTimer(3);
-//   Timer3: channel 0 on D2 or D10, channel 1 on D5 or D12
 
 void setup() {
   Serial.begin(115200);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
-  pinMode(motorPin, OUTPUT);
-
-  configureD21PWM();
 } 
 
 #define BUFFER_MAX 8
@@ -123,20 +97,4 @@ void execute_packet(int* packet) {
     default:
       break;
   }
-}
-
-void configureD21PWM() {
- zt3.configure(TC_CLOCK_PRESCALER_DIV1, // prescaler
-                TC_COUNTER_SIZE_16BIT,   // bit width of timer/counter
-                TC_WAVE_GENERATION_NORMAL_PWM // frequency or PWM mode
-                );
-  if (! zt3.PWMout(true, 0, 10)) {
-    Serial.println("Failed to configure PWM output");
-  }
-  if (! zt3.PWMout(true, 1, 12)) {
-    Serial.println("Failed to configure PWM output");
-  }
-  zt3.setPeriodMatch(SPEED_MAX, 0, 0);
-  zt3.setPeriodMatch(SPEED_MAX, 0, 1);
-  zt3.enable(true);
 }
