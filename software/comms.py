3d493c5ca6f974e904d215f1a743668df16a6c1d
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-04-06 20:34:47 +0800

    Update comms.py

--- software/comms.py
+++ software/comms.py
@@ -14,7 +14,7 @@ import sys
 NUM_BYTES = 3
 
 class Communications:
-    def __init__(self, usb, SCREEN_DIMENSIONS, cam_ids, BAUD_RATE):
+    def __init__(self, usb, cam_ids, BAUD_RATE):
         self.USB = usb
         self.cams = []
         for cam_id in cam_ids:
@@ -24,7 +24,6 @@ class Communications:
         self.ser.open()
     
     def kill_op(self):
-        sys.exit()
         end_packet = chr(9) + chr(254) + chr(255)
         self.encode_and_send(end_packet)
         
