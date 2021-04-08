7c803c3e9f58387e52eefa03b3c1db5dc814e24f
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-04-08 17:15:04 +0800

    Update sensors.py

--- software/sensors.py
+++ software/sensors.py
@@ -1,13 +1,19 @@
 import serial
 
 class Sensor:
-    def __init__(ID):
-        ser = serial.Serial(ID, 115200)
+    def __init__(self, ID):
+        self.ser = serial.Serial(ID, 115200)
     
-    def run():
+    def run(self):
         while True:
             self.line = self.ser.readline()
             if(self.line[1] == 'o'):
                 self.ori = self.line[4:].split()
+                print("o")
             elif(self.line[1] == 'g'):
                 sel.vel = self.line[4:].split()
+                print("g")
+
+if __name__ == "__main__":
+    sensor = Sensor("/dev/cu.usbmodem1431")
+    sensor.run()
