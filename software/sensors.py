d96a735ccfe4674b54caffdaa9de2f71fa0e5567
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-04-19 11:47:29 +0800

    Update sensors.py

--- software/sensors.py
+++ software/sensors.py
@@ -1,13 +1,29 @@
 import serial
+import pygame
+import time
 
 class Sensor:
-    def __init__(ID):
-        ser = serial.Serial(ID, 115200)
+    def __init__(self, ID):
+        self.ser = serial.Serial(ID, 115200)
     
-    def run():
+    def run(self):
         while True:
-            self.line = self.ser.readline()
-            if(self.line[1] == 'o'):
-                self.ori = self.line[4:].split()
-            elif(self.line[1] == 'g'):
-                sel.vel = self.line[4:].split()
+            #time.sleep(0.1)
+            try:
+                self.line = self.ser.readline()
+                #print(self.line)
+            except:
+                pass
+
+
+            #if(chr(self.line[1]) == 'o'):
+            #self.ori = self.line[4:].split(str = ',')
+            #print("o")
+            #elif(chr(self.line[1]) == 'g'):
+            #self.vel = self.line[4:].split(str = ',')
+            #print("g")
+
+
+if __name__ == "__main__":
+    sensor = Sensor("/dev/cu.usbmodem14111")
+    sensor.run()
