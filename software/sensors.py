a9aba7e0392b3a7961d787d76c343c7ffeb59b3d
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-04-19 11:57:40 +0800

    Update sensors.py

--- software/sensors.py
+++ software/sensors.py
@@ -5,16 +5,20 @@ import time
 class Sensor:
     def __init__(self, ID):
         self.ser = serial.Serial(ID, 115200)
+        self.lines = []
     
     def run(self):
         while True:
             #time.sleep(0.1)
             try:
                 self.line = self.ser.readline()
+                self.lines.append(self.line)
                 #print(self.line)
             except:
                 pass
 
+            if(len(self.lines) % 100000 == 1):
+                print(self.line)
 
             #if(chr(self.line[1]) == 'o'):
             #self.ori = self.line[4:].split(str = ',')
