251d3d64b3bae3476d7684da5d2ee17f51be9601
HaardikChopra <77324771+HaardikChopra@users.noreply.github.com>
2021-04-23 11:43:41 +0800

    Remove Multi-threading

--- software/main.py
+++ software/main.py
@@ -14,23 +14,26 @@ import camera
 import gui
 
 USB = ""
-SCREEN_DIMENSIONS = (500,500) # width, height
+SCREEN_H = 1280 # width
+SCREEN_W = 720 #height
 CAM_IDS = 0
 SPEED = 100
 BAUD_RATE = 115200
 
 def start():
-    comm = comms.Communications(USB, CAM_IDS, BAUD_RATE)
+    #comm = comms.Communications(USB, CAM_IDS, BAUD_RATE)
+    comm = None
     exit_prog = exit_program.Exit_Program(comm)
     cam = camera.Camera(CAM_IDS)
-    display = gui.GUI(SCREEN_DIMENSIONS, cam, comm, exit_prog, speed)
+    display = gui.GUI(SCREEN_H, SCREEN_W, cam, comm, exit_prog, SPEED)
     #controls = control.Control(comm, exit_prog, SPEED, SCREEN_DIMENSIONS)
     #gui_thread = threading.Thread(target=display.create)
-    display_thread = threading.Thread(target=display.create)
+    #display_thread = threading.Thread(target=display.create)
     #gui_thread.start()
-    display_thread.start()
+    #display_thread.start()
     #gui_thread.join()
-    display_thread.join()
+    #display_thread.join()
+    display.create()
     
     
 
